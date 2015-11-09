c0318af05d532d4fd1aff3359e0f4889d5726188
clonejo <clonejo@shakik.de>
2015-11-09 05:21:12 +0100

    bit of documentation

--- src/records.hrl
+++ src/records.hrl
@@ -2,6 +2,7 @@
 % This file is part of podcast_recoder, a project licensed under the terms of
 % the GNU Affero General Public License Version 3 (see LICENSE).
 
+% mnesia tables
 -record(file, {local_name, % used for URL and filename
                orig_url,
                last_fetch, % timestamp when the file has last been fetched (by us)
